b14e95c96f59c3de8c9beb4e4fd1e8498273f76c
Shravan Ramjathan <shravanramjathan@gmail.com>
2025-02-24 23:29:21 +0200

    Initial setup for a Calculator app with input fields and ViewModel
    This commit introduces the basic structure of a calculator application, including:
    
    -   **`CalculatorViewModel`:** A ViewModel to manage the state of two input fields (`input1`, `input2`) and the calculation result. It includes a `calculate` function to sum the two input numbers.
    -   **`libs.versions.toml`**: Update compose kotlin version
    -   **`MainActivity.kt`:**
        -   The main activity now includes a `Calculator` composable function.
        -   The `Calculator` composable displays two `OutlinedTextFields` for user input (`inputOne`, `inputTwo`).
    -   **`strings.xml`**: add two string resources to use on input fields
    +   -   Added `CalculatorViewModel` to manage the state of the calculator, including user inputs.
    +   -   Used string resources to add strings to the fields.
    -   **Added two fields** for two input numbers.

--- app/src/main/java/com/calculator/MainActivity.kt
+++ app/src/main/java/com/calculator/MainActivity.kt
@@ -5,17 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +42,7 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                 ) {
                     Column {
-                        InputOne()
+
                     }
                 }
             }
@@ -39,19 +51,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun InputOne() {
-    var text = remember { mutableStateOf("|") }
-    OutlinedTextField(
-        value = text.value,
-        onValueChange = { text.value = it },
-        label = { Text("|") },
+fun Calculator(viewModel: CalculatorViewModel = CalculatorViewModel(), modifier: Modifier = Modifier)  {
 
-    )
-    OutlinedTextField(
-        value = text.value,
-        onValueChange = { text.value = it },
-        label = { Text("Input Two") }
-    )
+    Box (){
+        Row( Modifier
+            .align(Alignment.Center)
+            .padding(8.dp), horizontalArrangement = Arrangement.SpaceEvenly) {
+            OutlinedTextField(
+                value = viewModel.inputOne,
+                onValueChange = { viewModel.inputOne = it },
+                label = { Text(stringResource(R.string.input_one)) },
+                modifier = Modifier.weight(1f)
+
+                )
+            Spacer(modifier = Modifier.width(16.dp)) // we can use this to add gaps in between
+            OutlinedTextField(
+                value = viewModel.inputTwo,
+                onValueChange = { viewModel.inputTwo = it },
+                label = { Text(stringResource(R.string.input_two)) },
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
 
 
@@ -64,9 +85,9 @@ fun GreetingPreview() {
                 .background(Color.Black)
                 .fillMaxSize()
         ) {
-            Column {
-                InputOne()
-            }
+
+                Calculator()
+
         }
 
     }

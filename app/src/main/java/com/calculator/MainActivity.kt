1e5fd7777c784ce36803239cc9f15a1ad68a3ccf
Shravan Ramjathan <shravanramjathan@gmail.com>
2025-02-24 19:31:45 +0200

    Added Text fields

--- app/src/main/java/com/calculator/MainActivity.kt
+++ app/src/main/java/com/calculator/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.calculator.ui.theme.CalculatorTheme
 
@@ -19,11 +24,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier
+                        .background(Color.Black)
+                        .fillMaxSize()
+                ) {
+                    Column {
+                        InputOne()
+                    }
                 }
             }
         }
@@ -31,17 +39,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun InputOne() {
+    var text = remember { mutableStateOf("|") }
+    OutlinedTextField(
+        value = text.value,
+        onValueChange = { text.value = it },
+        label = { Text("|") },
+
+    )
+    OutlinedTextField(
+        value = text.value,
+        onValueChange = { text.value = it },
+        label = { Text("Input Two") }
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalculatorTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize()
+        ) {
+            Column {
+                InputOne()
+            }
+        }
+
     }
 }

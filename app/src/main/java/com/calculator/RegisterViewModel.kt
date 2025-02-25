be632deabff6f250afd6b63f01a96a44e04ec516
Shravan Ramjathan <shravanramjathan@gmail.com>
2025-02-25 19:12:58 +0200

    Added RegisterViewModel and RegisteruiState classes.

---
+++ app/src/main/java/com/calculator/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.calculator
+
+
+
+class RegisterViewModel {
+
+}
---
+++ app/src/main/java/com/calculator/RegisteruiState.kt
@@ -0,0 +1,8 @@
+package com.calculator
+
+data class RegisteruiState(
+    val email: String,
+    val password:String,
+    val confirmPassword:String,
+    val dateOfBirth:String,
+)
